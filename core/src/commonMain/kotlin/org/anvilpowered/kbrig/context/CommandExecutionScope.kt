9c2614e4d068a004ac65f06cebab34a066c7f900
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-26 13:51:30 +0100

    Add some documentation to CommandExecutionScope

--- core/src/commonMain/kotlin/org/anvilpowered/kbrig/context/CommandExecutionScope.kt
+++ core/src/commonMain/kotlin/org/anvilpowered/kbrig/context/CommandExecutionScope.kt
@@ -20,6 +20,16 @@ import kotlin.coroutines.intrinsics.createCoroutineUnintercepted
 import kotlin.coroutines.intrinsics.suspendCoroutineUninterceptedOrReturn
 import kotlin.coroutines.resume
 
+/**
+ * Adds an execution block to this argument builder using the [CommandContextScopeDsl].
+ *
+ * The control-flow method [CommandExecutionScope.yield] and any of its specializations will exit the provided [block] early and
+ * lead to the provided value being returned during command execution.
+ * If [CommandExecutionScope.yield] is not invoked, [Command.SINGLE_SUCCESS] will be returned during command execution.
+ * Exceptions will be propagated to the caller of [Command.execute].
+ *
+ * See [CommandExecutionScope] for more information and examples.
+ */
 fun <S, B : ArgumentBuilder<S, B>> B.executesScoped(block: suspend CommandExecutionScope<S>.() -> Unit) =
     executesSuspending { context ->
         val scope = CommandExecutionScopeImpl(context)
@@ -28,6 +38,102 @@ fun <S, B : ArgumentBuilder<S, B>> B.executesScoped(block: suspend CommandExecut
         scope.await()
     }
 
+/**
+ * A scope that allows for the extraction of arguments from the command context and manipulation of the control-flow outside
+ * the main command execution block.
+ *
+ * ## Example usage
+ *
+ * ### Creating your own DSL
+ *
+ * A simple extraction of the current source, or `null` if the source is not a player.
+ * Does not interrupt control-flow and does not send a message to the source.
+ *
+ * ```kt
+ * @CommandContextScopeDsl
+ * fun CommandContext.Scope<CommandSource>.extractPlayerSourceOrNull(): Player? =
+ *     context.source as? Player
+ * ```
+ *
+ * A more complex extraction of the current source; exits early and sends a message to the source if not a player.
+ * Interrupts control-flow and returns 0 during command execution with [CommandExecutionScope.yieldError].
+ * Note that this replaces the traditional control-flow statement with `return 0` in the main command execution block.
+ *
+ * In this case, the Kotlin compiler is able to smart-cast the source to a non-null Player after the null-check because none
+ * of the yield methods return.
+ *
+ * ```kt
+ * @CommandContextScopeDsl
+ * suspend fun CommandExecutionScope<CommandSource>.extractPlayerSource(): Player {
+ *     val player = extractPlayerSourceOrNull()
+ *     if (player == null) {
+ *         context.source.sendMessage(
+ *             Component.text(
+ *                 "You must be a player to use this command!",
+ *                 NamedTextColor.RED
+ *             )
+ *         )
+ *         yieldError()
+ *     }
+ *     return player
+ * }
+ * ```
+ *
+ * Extracts a player argument from the command context; exits early and sends a message to the source if the player is not found.
+ *
+ * ```kt
+ * @CommandContextScopeDsl
+ * suspend fun CommandExecutionScope<CommandSource>.extractPlayerArgument(
+ *     proxyServer: ProxyServer,
+ *     argumentName: String = "player",
+ * ): Player {
+ *     val playerName = context.get<String>(argumentName)
+ *     val player = proxyServer.getPlayer(playerName).getOrNull()
+ *     if (player == null) {
+ *         context.source.sendMessage(
+ *             Component.text()
+ *                 .append(Component.text("Player with name ", NamedTextColor.RED))
+ *                 .append(Component.text(playerName, NamedTextColor.GOLD))
+ *                 .append(Component.text(" not found!", NamedTextColor.RED))
+ *                 .build(),
+ *         )
+ *         yieldError()
+ *     }
+ *     return player
+ * }
+ * ```
+ *
+ * ### Using the DSL
+ *
+ * The strength in this approach lies in the ability for invoked methods
+ * to interrupt control-flow.
+ * Usually, it is necessary to introduce a control-flow statement in the main command execution block;
+ * for example, to check whether a player exists and exit early if not.
+ *
+ * The following example shows how to use the DSL to create a command that sends a private message to another player.
+ * It uses the examples defined above to extract the source player and the target player.
+ *
+ * ```kt
+ * ArgumentBuilder.required<CommandSource, String>("message", StringArgumentType.GreedyPhrase)
+ *     .executesScoped {
+ *         // exits early and sends a message to the source if not a player
+ *         val sourcePlayer = extractPlayerSource()
+ *         // exits early and sends a message to the source if the player is not found
+ *         val targetPlayer = extractPlayerArgument(proxyServer)
+ *
+ *         if (sourcePlayer.uniqueId == targetPlayer.uniqueId) {
+ *             context.source.sendMessage(PluginMessages.messageSelf)
+ *             yieldError() // exits the block early and returns 0 during command execution
+ *         }
+ *         privateMessageService.sendMessage(
+ *             sourcePlayer,
+ *             targetPlayer,
+ *             MiniMessage.miniMessage().deserialize(context.get<String>("message")),
+ *         )
+ *         yieldSuccess()
+ *     }.build(),
+ * ```
+ */
 interface CommandExecutionScope<S> : CommandContext.Scope<S> {
     @CommandContextScopeDsl
     suspend fun yield(value: Int): Nothing
